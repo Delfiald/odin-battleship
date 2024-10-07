b0bf119d84fbf237b684929bb1903bd4803e3187
M.Aldi Gunawan <aldigunawan1808@gmail.com>
2024-10-07 23:45:12 +0700

    Implement ship, gameboard, player functionality
    - Add Ship component for creating ships with hit tracking
    - Implement Gameboard for managing ship placement and attacks
    - Create Player component to manage player type (human/AI)
    - Add Player functionality for attacking gameboard

---
+++ src/components/ship.js
@@ -0,0 +1,27 @@
+const Ship = (shipLength) => {
+  const shipProperty = {
+    length: shipLength,
+    hit: 0,
+    sunk: false
+  }
+  
+  const hit = () => {
+    shipProperty.hit += 1;
+  }
+
+  const isSunk = () => {
+    if (shipProperty.hit >= shipProperty.length) {
+      shipProperty.sunk = true;
+    }
+
+    return shipProperty.sunk
+  }
+
+  return {
+    shipProperty,
+    hit,
+    isSunk
+  }
+}
+
+export default Ship
